baf9dc0286697fbe88367390cb3e4f09d967f266
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-11-11 01:51:55 +0300

    ilk databinding activitye uygulandı

--- app/src/main/java/com/emrealtunbilek/databindingjava/MainActivity.java
+++ app/src/main/java/com/emrealtunbilek/databindingjava/MainActivity.java
@@ -1,14 +1,31 @@
 package com.emrealtunbilek.databindingjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.emrealtunbilek.databindingjava.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    ActivityMainBinding mainBinding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main); //activity_urunler ActivityUrunlerBinding
+
+        Ogrenci ogr1=new Ogrenci(1,"emre");
+        Ogrenci ogr2=new Ogrenci(2,"hasan");
+
+       /* mainBinding.tvIsim.setText(ogr1.getIsim());
+        mainBinding.ogrNo.setText(""+ogr1.getNo());*/
+
+       mainBinding.setOgrenci(ogr2);
+
+
     }
 }

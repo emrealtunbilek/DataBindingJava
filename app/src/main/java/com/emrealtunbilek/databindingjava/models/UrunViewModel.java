425c1220e5582677367fd4ef9a1c5122d05257e1
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-11-14 23:44:02 +0300

    observable açıklaması yapıldı ve alt yapısı hazırlandı

---
+++ app/src/main/java/com/emrealtunbilek/databindingjava/models/UrunViewModel.java
@@ -0,0 +1,25 @@
+package com.emrealtunbilek.databindingjava.models;
+
+public class UrunViewModel {
+
+    private Urun urun;
+    private int miktar;
+
+
+
+    public Urun getUrun() {
+        return urun;
+    }
+
+    public void setUrun(Urun urun) {
+        this.urun = urun;
+    }
+
+    public int getMiktar() {
+        return miktar;
+    }
+
+    public void setMiktar(int miktar) {
+        this.miktar = miktar;
+    }
+}
